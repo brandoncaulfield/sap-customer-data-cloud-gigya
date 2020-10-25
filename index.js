fba49655c417a6e857e81a0479ad5925db29c426
brandoncaulfield <19891236+brandoncaulfield@users.noreply.github.com>
2020-10-25 21:15:36 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// const gigya = window.gigya;
+
+// const testFunction = () => {
+//   gigya.accounts.showScreenSet({
+//     screenSet: "NewRaas4nov15-RegistrationLogin",
+//     customButtons,
+//   });
+// };
